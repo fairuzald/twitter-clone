24047825a59afad92ca406cb1de34c33d29377d6
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-10 22:53:32 +0700

    Add extend colors for tailwind config and trigger commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "black-soft": "#1C1A17",
+        "black": "#000000",
+        "red": "#EF1B27",
+        "vermillion": "#FF2B06",
+        "yellow": "#FECC4E",
+        "gray": "#D7D2D0",
+        "yellow-pale": "#FFDB80",
+        "red-pale": "#F56870",
+        "green": "#3FB160",
+        "gold": "#FEB20E",
+        "blue": "#046EE7",
+        "purple": "#7B61FF",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
