c6a4e2b2d3e34f9e8b74384258a46b4bd025e3b0
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-12 00:13:17 +0700

    Preparing component icons for building purposes

--- src/components/icons/Layout.tsx
+++
@@ -1,7 +0,0 @@
-interface LayoutProps {
-  children: React.ReactNode;
-}
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div className="h-screen"></div>;
-};
-export default Layout;
