9e4f9af79eaee231399529017a324612f8838bba
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-17 01:10:32 +0700

    Fixing design ui on users desktop

---
+++ src/components/UserHero.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import Avatar from "./Avatar";
+interface UserHeroProps {
+  user: Record<any, string>;
+}
+const UserHero: React.FC<UserHeroProps> = ({ user }) => {
+  return (
+    <div className="relative flex h-60 w-full bg-[#333639]">
+      {/* Display the user's cover image */}
+      {user && user.coverImage && (
+        <Image
+          src={user.coverImage}
+          alt={"Cover Image"}
+          width="1920"
+          height="1080"
+          className="0bject-center h-full w-full overflow-hidden object-cover"
+        />
+      )}
+      {/* Display the user's avatar */}
+      <div className="absolute -bottom-16 left-4">
+        <Avatar userId={user?.id as string} size="large" isBorder />
+      </div>
+    </div>
+  );
+};
+
+export default UserHero;
