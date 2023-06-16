0912a729bd3a19364f826c6dc7b4f658f298ac73
Moh Fairuz Alauddin Yahya <112830919+fairuzald@users.noreply.github.com>
2023-06-17 01:10:00 +0700

    Fixing design ui on notification desktop

--- src/pages/notification.tsx
+++ src/pages/notification.tsx
@@ -3,34 +3,40 @@ import PageHead from "@/components/PageHead";
 import TwitterIcon from "@/components/icons/TwitterIcon";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useNotifications from "@/hooks/useNotification";
-import type { NextPageContext } from "next";
-import { getSession } from "next-auth/react";
 import { useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 
 const Notifications = () => {
+  // Using the useCurrentUser hook to get the current user data and its loading state
   const {
     data: currentUser,
     mutate: mutateCurrentUser,
     isLoading: currentUserLoading,
   } = useCurrentUser();
+
+  // Using the useNotifications hook to get the notifications data and its loading state
   const { data: fetchedNotifications = [], isLoading } = useNotifications(
     currentUser?.id
   );
+
+  // Run this effect when the mutateCurrentUser function changes
   useEffect(() => {
     mutateCurrentUser();
   }, [mutateCurrentUser]);
 
   return (
     <>
+      {/* Page metadata */}
       <PageHead
-        title="Twitter Clone"
+        title="Notifications | Twitter Clone"
         websiteName="Fairuz Twitter Clone"
         description="Twitter clone made by fairuz"
         faviconUrl="/logo-twitter-clone.png"
         logoUrl="https://www.datocms-assets.com/102850/1686900586-logo-twitter-clone.png"
       />
+
       {isLoading || currentUserLoading ? (
+        // Show loading spinner if either the notifications or current user data is loading
         <div className="flex h-screen w-full items-center justify-center">
           <ClipLoader
             color="#308CD8"
@@ -41,14 +47,20 @@ const Notifications = () => {
           />
         </div>
       ) : (
+        // No notifications found
         <>
           {fetchedNotifications.length === 0 ? (
+            // Show "No notifications" message if there are no notifications
             <div className="p-6 text-center text-xl text-neutral-600">
               No notifications
             </div>
           ) : (
+            // Notifications found
             <>
+              {/* Header component */}
               <Header showArrowButton label="Notifications" />
+
+              {/* Render the list of notifications */}
               <div className="flex flex-col">
                 {fetchedNotifications.map(
                   (notification: Record<string, any>) => (
